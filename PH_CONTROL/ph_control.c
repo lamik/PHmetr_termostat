6ad7633a9edd3205111e8c1a504c3f95ede5e163
Mateusz Salamon <mateusz.salamon91@gmail.com>
2017-01-15 12:57:06 +0100

    Add files for ph controler.
    	Now onlu kH value to build kH menu screens.

---
+++ PH_CONTROL/ph_control.c
@@ -0,0 +1,10 @@
+/*
+ * ph_control.c
+ *
+ *  Created on: 15 sty 2017
+ *      Author: Mateusz Salamon
+ */
+#include <avr/io.h>
+
+#include "ph_control.h"
+
---
+++ PH_CONTROL/ph_control.h
@@ -0,0 +1,13 @@
+/*
+ * ph_control.h
+ *
+ *  Created on: 15 sty 2017
+ *      Author: Mateusz Salamon
+ */
+
+#ifndef PH_CONTROL_PH_CONTROL_H_
+#define PH_CONTROL_PH_CONTROL_H_
+
+uint8_t kH_val;
+
+#endif /* PH_CONTROL_PH_CONTROL_H_ */
