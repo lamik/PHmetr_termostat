16f5d17b1d464258826b453329ffea96b26daed8
Mateusz Salamon <mateusz.salamon91@gmail.com>
2017-01-21 19:25:42 +0100

    pH measurement basen on average of 10 samples.
    	Circle buffer getting 10 samples from ADC and calculate average.
    	First 10 samples are not correct because average is lower than real. But goes to right average in 4 seconds.

--- PH_CONTROL/ph_control.c
+++ PH_CONTROL/ph_control.c
@@ -40,19 +40,26 @@ void Ph_controler_get_pH()
 {
 	if(!Timer_pH)
 	{
-		pH_ADC_mid = ADC;
+		static uint16_t adc_mes[10];
+		static uint8_t idx;
+		uint8_t i;
+
+		adc_mes[idx++] = ADC;
+		if(idx>9) idx = 0;
+
+		pH_ADC_mid = 0; //calculate average from 10 mes
+		for(i = 0; i<10 ; i++) pH_ADC_mid += adc_mes[i];
+		pH_ADC_mid /= 10;
+
 		ADCSRA |= (1<<ADSC);
-		double tmp = abs(pH_calib_7pH - pH_calib_4pH);
 
-//		pH_ADC = (3*ph_temp) - (7*pH_calib_4pH) + (4*pH_calib_7pH);
-
-		long double pH_ADC_tmp = abs((3*pH_ADC_mid) - (7*pH_calib_4pH) + (4*pH_calib_7pH));
+		float tmp = abs(pH_calib_7pH - pH_calib_4pH);
+		float pH_ADC_tmp = abs((3*pH_ADC_mid) - (7*pH_calib_4pH) + (4*pH_calib_7pH));
 
 		pH_ADC_tmp /= tmp;
-
 		pH_all = pH_ADC_tmp*100;
 
-		Timer_pH = 100;
+		Timer_pH = 25;
 	}
 }
 
