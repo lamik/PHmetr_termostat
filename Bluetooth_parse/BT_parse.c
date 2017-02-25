6c0e83499bc4ec62036c7fb940f8c64870d8e2f4
Mateusz Salamon <mateusz.salamon91@gmail.com>
2017-02-25 14:36:27 +0100

    Defined parsing functions, first AT commands.

--- Bluetooth_parse/BT_parse.c
+++ Bluetooth_parse/BT_parse.c
@@ -6,17 +6,159 @@
  */
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../UART_IRQ/mkuart.h"
+#include "BT_parse.h"
 #include "../LCD_buf/lcd44780.h"
 
-void parse_uart_data( char * pBuf ) {
 
-	// wywietlamy odebrane polecenia AT na LCD
-	lcd_locate(0,0);
-	lcd_locate(1,0);
-	lcd_str("                ");
-	lcd_locate(1,0);
-	lcd_str("[");
-	lcd_str( pBuf );
-	lcd_str("]");
+#define AT_CNT 	10	// iloæ poleceñ AT
 
+//----------- tablica z poleceniami AT i wskaŸnikami funkcji do ich obs³ugi --------------------
+const TATCMD polecenia_at[AT_CNT] PROGMEM =
+{
+	// { at_cmd } , { wskaŸnik do funkcji obs³ugi at },
+		{"AT", 		at_service},
+		{"ATI", 	ati_service},
+
+		{"AT+INF", 	at_inf_service}, //request for all parameters
+
+		{"AT+KH", 	at_kh_service}, //KH setting
+
+		{"AT+TMP", 	at_tmp_service}, //temperature setting
+		{"AT+TMH", 	at_tmh_service}, //temperature histerese setting
+		{"AT+TMK", 	at_tmk_service}, //critical temperature setting
+
+		{"AT+PH", 	at_php_service}, //pH setting
+		{"AT+PHH", 	at_phh_service}, //pH histerese setting
+		{"AT+PHK", 	at_phk_service}, //critical pH setting
+};
+
+void parse_uart_data( char * pBuf )
+{
+	int8_t (*_at_srv)(uint8_t inout, char * data);
+
+		char * cmd_wsk;
+		char * reszta;
+		uint8_t i=0, len;
+
+		if ( strpbrk(pBuf, "=?"))	{
+			// obs³uga poleceñ AT we/wy + parametry
+
+			if ( strpbrk(pBuf, "?"))	{
+				// zapytania do uk³adu w postaci: AT+CMD?
+
+				cmd_wsk = strtok_r(pBuf, "?", &reszta);
+				len = strlen(cmd_wsk);
+				for(i=0;i<AT_CNT;i++) {
+					if ( len && 0 == strncasecmp_P(cmd_wsk, polecenia_at[i].polecenie_at, len) ) {
+						if( pgm_read_word(polecenia_at[i].polecenie_at) ) { // <--- UWAGA! w tekœcie ksi¹¿ki zabrak³o pgm_read_word()
+							_at_srv = (void *)pgm_read_word( &polecenia_at[i].at_service );
+							if( _at_srv) {
+								if( _at_srv( 0, reszta ) < 0 ) uart_puts("ERROR\r\n");
+							}
+						}
+						uart_puts("\r\n");
+						break;
+					}
+				}
+
+			} else {
+				// ustawienia uk³adu w postaci: AT+CMD=parametry
+
+				cmd_wsk = strtok_r(pBuf, "=", &reszta);
+				len = strlen(cmd_wsk);
+				for(i=0;i<AT_CNT;i++) {
+					if ( len && 0 == strncasecmp_P(cmd_wsk, polecenia_at[i].polecenie_at, len) ) {
+						if( pgm_read_word(polecenia_at[i].polecenie_at) ) { // <--- UWAGA! w tekœcie ksi¹¿ki zabrak³o pgm_read_word()
+							_at_srv = (void *)pgm_read_word( &polecenia_at[i].at_service );
+							if( _at_srv && ! _at_srv( 1, reszta ) ) uart_puts("OK\r\n");
+							else uart_puts("ERROR\r\n");
+						}
+						break;
+					}
+				}
+			}
+
+		} else {
+			// obs³uga poleceñ AT bez parametrów
+
+			if( 0 == pBuf[0] ) uart_puts("\r\n");	// reakcja na znak CR lub CRLF z terminala
+			else {
+				for(i=0;i<AT_CNT;i++) {
+					if ( 0 == strncasecmp_P(pBuf, polecenia_at[i].polecenie_at, strlen(pBuf)) ) {
+						if( pgm_read_word(polecenia_at[i].polecenie_at) ) { // <--- UWAGA! w tekœcie ksi¹¿ki zabrak³o pgm_read_word()
+							_at_srv = (void *)pgm_read_word( &polecenia_at[i].at_service );
+							if( _at_srv) _at_srv(2,0);
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		if( AT_CNT == i ) uart_puts("ERROR\r\n");
+}
+
+int8_t at_service(uint8_t inout, char * params)
+{
+	uart_puts("OK\r\n");
+	return 0;
+}
+
+int8_t ati_service(uint8_t inout, char * params)
+{
+	uart_puts("pH Controller\r\n");
+	return 0;
+}
+
+int8_t at_inf_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_kh_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_tmp_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_tmh_service( uint8_t inout, char * params )
+{
+
+	return 0;
+}
+
+int8_t at_tmk_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_php_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_phh_service(uint8_t inout, char * params)
+{
+
+	return 0;
+}
+
+int8_t at_phk_service( uint8_t inout, char * params )
+{
+
+	return 0;
 }
--- Bluetooth_parse/BT_parse.h
+++ Bluetooth_parse/BT_parse.h
@@ -8,6 +8,27 @@
 #ifndef BLUETOOTH_PARSE_BT_PARSE_H_
 #define BLUETOOTH_PARSE_BT_PARSE_H_
 
+// definicja typu strukturalnego
+typedef struct {
+	char polecenie_at[8];
+	int8_t (* at_service)(uint8_t inout, char * params);
+} TATCMD;
+
+
+// deklaracje zmiennych zewnêtrznych
+extern const TATCMD polecenia_at[];
+
 void parse_uart_data( char * pBuf );
+
+int8_t at_service(uint8_t inout, char * params);
+int8_t ati_service(uint8_t inout, char * params);
+int8_t at_inf_service(uint8_t inout, char * params);
+int8_t at_kh_service(uint8_t inout, char * params);
+int8_t at_tmp_service(uint8_t inout, char * params);
+int8_t at_tmh_service( uint8_t inout, char * params );
+int8_t at_tmk_service(uint8_t inout, char * params);
+int8_t at_php_service(uint8_t inout, char * params);
+int8_t at_phh_service(uint8_t inout, char * params);
+int8_t at_phk_service( uint8_t inout, char * params );
 
 #endif /* BLUETOOTH_PARSE_BT_PARSE_H_ */
